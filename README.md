729579241ab86f4d86bb8dc7cf4f94cf48ddbee6
Thais Nara <nara.silva@grendene.com>
2024-06-19 23:17:13 -0300

    Ajustando o Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TabNewsClone
+
 Clone do TabNews, curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-  return <h1>Olá Mundo! 👩🏻‍🚀</h1>
+function Home() {
+  return <h1>Olá Mundo! 👩🏻‍🚀</h1>;
 }
 
 export default Home;
